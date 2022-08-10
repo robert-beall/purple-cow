d70e6924b811f9d1e3cee36c808d9186ecd4ba81
robert-beall <rbeall@smcm.edu>
2022-08-09 22:10:34 -0400

    Name changes for component files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './components/counter.js';
+import Counter from './components/Counter.js';
 
 function App() {
   return (
--- src/components/counter.js
+++ src/components/counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import EventButton from './eventbutton.js';
+import EventButton from './EventButton.js';
 
 export default class Display extends React.Component {
 
